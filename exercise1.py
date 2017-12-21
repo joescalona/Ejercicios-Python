b0ad122c27fcbb57d11e6faf3a5147e498357311
Joaquin <33739654+joescalona@users.noreply.github.com>
2017-12-21 00:58:21 -0300

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -3,9 +3,9 @@
 # PROBLEMA: ESCRIBIR UN PROGRAMA QUE RECIBA NOMBRE Y EDAD Y DECIR
 # EN QUÉ AÑO EL USUARIO CUMPLIRÁ 100 AÑOS
 
-Nom=raw_input('Hola, cual es tu nombre? \n') #nom=nombre
+Nom=raw_input('Hola, cual es tu nombre? \n')
 print('Hola '+Nom+' !')
-x=int(input('Que edad tienes?')) #x=años que tiene
+x=int(input('Que edad tienes?'))
 y=int(input('En que año estamos?')) #y=año actual 
 print('Entonces en el año '+str((100-x)+y)+' cumpliras 100 años')
 
